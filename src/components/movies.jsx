49798a6944cf40f6c5235e12ec79c46ebaabadae
hanymoh <hanymoh@msn.com>
2019-09-07 16:40:08 +0200

    Implementing Sorting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,17 +5,19 @@ import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
+import _ from 'lodash';
 
 class Movies extends Component {
     state = {
         movies: [],
         genres: [],
         currentPage: 1,
-        pageSize: 4
+        pageSize: 4,
+        sortColumn: { path: 'title', order: 'asc' }
     }
 
     componentDidMount() {
-        const genres = [{_id: '', name: 'All Genres' }, ...getGenres()]
+        const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()]
         this.setState({ movies: getMovies(), genres: genres });
     }
 
@@ -41,19 +43,29 @@ class Movies extends Component {
     };
 
     handleSort = path => {
-        console.log(path);
+        const sortColumn = { ...this.state.sortColumn };
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }    
+        this.setState({ sortColumn });
     }
 
     render() {
         const { length: count } = this.state.movies;
-        const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
+        const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state;
         if (count === 0)
             return <p>There are no movies in database.</p>;
 
         const filtered = selectedGenre && selectedGenre._id
             ? allMovies.filter(m => m.genre._id === selectedGenre._id)
             : allMovies;
-        const movies = paginate(filtered, currentPage, pageSize);
+
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize);
 
         return (
             <React.Fragment>
@@ -62,7 +74,7 @@ class Movies extends Component {
                         <ListGroup
                             items={this.state.genres}
                             selectedItem={this.state.selectedGenre}
-                            onItemSelect={this.handleGenreSelect} 
+                            onItemSelect={this.handleGenreSelect}
                         />
                     </div>
                     <div className="col">
