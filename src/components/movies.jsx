889e12d25a9a6129d5adbed7a9bbb1fdb4d27892
HANYMOH <hanymoh@msn.com>
2019-09-13 09:41:32 +0200

    Sorting- Extracting a Method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -43,15 +43,18 @@ class Movies extends Component {
     };
 
     handleSort = sortColumn => {
-            
+
         this.setState({ sortColumn });
     }
 
-    render() {
-        const { length: count } = this.state.movies;
-        const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state;
-        if (count === 0)
-            return <p>There are no movies in database.</p>;
+    getPageData = () => {
+        const {
+            pageSize,
+            currentPage,
+            sortColumn,
+            selectedGenre,
+            movies: allMovies
+        } = this.state;
 
         const filtered = selectedGenre && selectedGenre._id
             ? allMovies.filter(m => m.genre._id === selectedGenre._id)
@@ -60,6 +63,16 @@ class Movies extends Component {
         const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
         const movies = paginate(sorted, currentPage, pageSize);
+        return { totalCount: filtered.length, data: movies };
+    }
+
+    render() {
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, sortColumn } = this.state;
+        if (count === 0)
+            return <p>There are no movies in database.</p>;
+
+        const { totalCount, data: movies } = this.getPageData();
 
         return (
             <React.Fragment>
@@ -72,7 +85,7 @@ class Movies extends Component {
                         />
                     </div>
                     <div className="col">
-                        <p>Showing {filtered.length} movies in the database.</p>
+                        <p>Showing {totalCount} movies in the database.</p>
                         <MoviesTable
                             movies={movies}
                             sortColumn={sortColumn}
@@ -81,7 +94,7 @@ class Movies extends Component {
                             onSort={this.handleSort}
                         />
                         <Pagination
-                            itemCount={filtered.length}
+                            itemCount={totalCount}
                             pageSize={pageSize}
                             currentPage={currentPage}
                             onPageChange={this.handlePageChange}
