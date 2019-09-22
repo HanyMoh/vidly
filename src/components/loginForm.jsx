3140d3214a1ad976291066a157555036445fb7de
HanyMoh <hanymoh@msn.com>
2019-09-22 09:16:43 +0200

    Handling Multiple Inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -7,16 +7,17 @@ class LoginForm extends Component {
     handleSubmit = e => {
         e.preventDefault();
         // Call the server
-        const username = this.username.current.value;
+        // const username = this.username.current.value;
         console.log("Submitted");
     }
-    handleChange = e => {
+    handleChange = ({currentTarget: input}) => {
         const account = { ...this.state.account }
-        account.username = e.currentTarget.value;
+        account[input.name] = input.value;
         this.setState({ account });
     }
 
     render() {
+        const { account } = this.state;
         return (
             <div>
                 <h1>Login</h1>
@@ -25,16 +26,24 @@ class LoginForm extends Component {
                         <div className="div form-group">
                             <label htmlFor="username">Username</label>
                             <input
-                                value={this.state.account.username}
+                                value={account.username}
                                 onChange={this.handleChange}
                                 id="username"
+                                name="username"
                                 type="text"
                                 className="form-control"
                             />
                         </div>
                         <div className="div form-group">
                             <label htmlFor="password">Password</label>
-                            <input id="password" type="text" className="form-control" />
+                            <input
+                                value={account.password}
+                                onChange={this.handleChange}
+                                id="password"
+                                name="password"
+                                type="text"
+                                className="form-control"
+                            />
                         </div>
                         <button className="btn btn-primary">Login</button>
                     </div>
