64767a276f7fc6edaa0e226f0d41b1edb1208be9
HanyMoh <hanymoh@msn.com>
2019-09-23 09:37:47 +0200

    Validation on Change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -31,10 +31,28 @@ class LoginForm extends Component {
         // Call the server
         console.log("Submitted");
     }
+
+    validateProperty = ({ name, value }) => {
+        if (name === 'username') {
+            if (value.trim() === '') return 'Username is required.';
+            // ...
+        }
+        if (name === 'password') {
+            if (value.trim() === '') return 'Password is required.';
+            // ...
+        }
+    }
+
     handleChange = ({ currentTarget: input }) => {
-        const account = { ...this.state.account }
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(input);
+
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
+        const account = { ...this.state.account };
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account, errors });
     }
 
     render() {
