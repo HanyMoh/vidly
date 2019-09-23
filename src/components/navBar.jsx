2224a1e906428e76fb47ff9e5d72cca788f71c34
HanyMoh <hanymoh@msn.com>
2019-09-23 16:53:55 +0200

    Register Form

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -12,22 +12,27 @@ const NavBar = () => {
                 <ul className="navbar-nav">
                     <li className="nav-item active">
                         <NavLink className="nav-link" to="/movies">
-                            Movies 
+                            Movies
                         </NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/customers">
-                            Customers 
+                            Customers
                         </NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/rentals">
-                            Rentals 
+                            Rentals
                         </NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/login">
-                            Login 
+                            Login
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/register">
+                            Register
                         </NavLink>
                     </li>
                 </ul>
