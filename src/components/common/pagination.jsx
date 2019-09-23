989db91682af213c45a31b12e6af4b206e221241
HanyMoh <hanymoh@msn.com>
2019-09-23 09:18:33 +0200

    Displaying Validation Errors

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -12,11 +12,11 @@ const Pagination = (props) => {
         <ul className="pagination">
             {pages.map(page => (
                 <li key={page} className={page === currentPage ? "page-item active" : "page-item"}>
-                    <a 
+                    <button 
                         className="page-link"
                         onClick={() => onPageChange(page)}
                     >{page}
-                    </a>
+                    </button>
                 </li>
             ))}
             
