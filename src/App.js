e49565275c4be54039083b1731a9bfae7d537451
HanyMoh <hanymoh@msn.com>
2019-09-27 15:17:56 +0200

    Adding Http and Log Services

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Movies from './components/movies';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
@@ -8,16 +9,18 @@ import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
+      <ToastContainer />
       <NavBar />
       <main className="container">
         <Switch>
-        <Route path='/register' component={RegisterForm}></Route>
-        <Route path='/login' component={LoginForm}></Route>
+          <Route path='/register' component={RegisterForm}></Route>
+          <Route path='/login' component={LoginForm}></Route>
           <Route path='/movies/:id' component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
