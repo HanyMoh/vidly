06c5a2709a6755ae6545b474dd71a24849a04803
HanyMoh <hanymoh@msn.com>
2019-09-28 22:52:36 +0200

    Fixing url

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,11 +1,11 @@
 import http from './httpServices';
 
-const apiEndPoint = "http://localhost:5000/movies.json";
+const apiEndPoint = "http://localhost:5000/movies";
 
 export function getMovies() {
-  return http.get(apiEndPoint);
+  return http.get(apiEndPoint + ".json");
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndPoint + "/" + movieId);
+  return http.delete(apiEndPoint + "/" + movieId + ".json");
 }
