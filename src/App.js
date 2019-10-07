bf1c7e842834b169719532894f00a55f4a62301b
HanyMoh <hanymoh@msn.com>
2019-10-07 17:36:38 +0200

    Getting the Current User

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import jwtDecode from 'jwt-decode';
 import Movies from './components/movies';
 import MovieForm from './components/movieForm';
 import Customers from './components/customers';
@@ -12,26 +13,39 @@ import RegisterForm from './components/registerForm';
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path='/register' component={RegisterForm}></Route>
-          <Route path='/login' component={LoginForm}></Route>
-          <Route path='/movies/:id' component={MovieForm}></Route>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from='/' exact to='/movies' />
-          <Redirect to='/not-found' />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {};
+  componentDidMount() {
+    try {
+
+      const jwt = localStorage.getItem('token');
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {
+
+    }
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
+        <main className="container">
+          <Switch>
+            <Route path='/register' component={RegisterForm}></Route>
+            <Route path='/login' component={LoginForm}></Route>
+            <Route path='/movies/:id' component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from='/' exact to='/movies' />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
