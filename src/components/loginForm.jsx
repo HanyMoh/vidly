8d6c4717b7813e4b75d1737c07011eda4e103d55
HanyMoh <hanymoh@msn.com>
2019-10-08 22:57:37 +0200

    Redirecting after Login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Redirect} from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
 import auth from '../services/authService';
@@ -22,8 +23,9 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
       await auth.login(data.username, data.password);
-      
-      window.location = "/"
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if ((ex.response && ex.response.status === 422) ||
         (ex.response && ex.response.status === 400)) {
@@ -35,6 +37,7 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
     return (
       <div>
         <h1>Login</h1>
